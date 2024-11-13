25d3561603184b3449f14a882513bcc05decf7ac
Randell Altea <163165534+rundell09@users.noreply.github.com>
2024-11-12 20:02:41 -0800

    program.py

--- program.py
+++ program.py
@@ -16,3 +16,16 @@ while choice.upper() != 'D':
  clearRec()
  elif choice.upper() == 'D':
  print("Thank you!")
+def addRec():
+ file = open(filename, 'r')
+ name = input("Enter Name: ")
+ email = input("Enter Email: ")
+ addr = input("Enter Address: ")
+ with open(filename,'a') as file:
+ file.write(name + ", " + email + ", " + addr + "\n")
+ file.close()
+ def viewRec():
+ file = open(filename, 'r')
+ with open(filename, 'r') as read_file:
+ print(read_file.read())
+ read_file.close()
